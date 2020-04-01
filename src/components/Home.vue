639754168b287e67b09dcd23091858a2a2e195f1
Olivier Crété <o.crete@effy.fr>
2020-04-01 18:55:11 +0200

    remove useless stuff

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Hello :)',
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
