8a316fbf0ffde6f12be758a561a1279fba615612
Jérôme Crété <jerome1.crete@epitech.eu>
2020-04-18 17:46:05 +0000

    Players table

--- src/components/pages/Game.vue
+++ src/components/pages/Game.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container">
-    <game-form/>
+    <game-form @sync="SyncData"/>
     <game-quick-score
-      @quick-score="onQuickScore"
       v-if="quickScoreOptions"
-      :options="quickScoreOptions"
+      @quick-score="onQuickScore"
+      :options="quickScoreOptions"/>
+    <player-table
+      @sync="SyncData"
+      :players="players"
+      :step="step"
     />
   </div>
 </template>
@@ -12,15 +16,25 @@
 <script lang="ts">
 import GameForm from '@/components/game/GameForm';
 import GameQuickScore from '@/components/game/GameQuickScore';
+import PlayerTable from '@/components/game/PlayerTable';
+import Player from '@/structs/player.class';
 
 export default {
   name: 'Game',
   components: {
     GameForm,
     GameQuickScore,
+    PlayerTable,
   },
   data() {
     return {
+      players: [
+        new Player("Jér"),
+        new Player('Oli'),
+        new Player('Sym'),
+        new Player('Xav'),
+      ],
+      step: 10,
       quickScoreOptions: [...Array(11).keys()],
     };
   },
@@ -28,6 +42,9 @@ export default {
     onQuickScore(points: number): void {
       // TODO: update current player points
       console.log("update score", points)
+    },
+    SyncData(property: string, value: any): any {
+      this[property] = value;
     },
   },
 };
