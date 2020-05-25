b6dc24fb835e15c91a15dc8d7e6b10bece84f960
Olivier Crété <o.crete@effy.fr>
2020-05-25 21:18:19 +0200

    add events on quickscore use

--- src/components/pages/Game.vue
+++ src/components/pages/Game.vue
@@ -93,6 +93,10 @@ export default {
   methods: {
     onQuickScore(points: number): void {
       this.currentPlayer.addPoints(points);
+      this.emitLogEvent({
+        type: EventsType.POINTS,
+        data: { player: this.currentPlayer.name, points }
+      });
       this.nextPlay();
     },
     nextPlay(): void {
